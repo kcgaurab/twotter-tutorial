72c97d3633c9dd3db192c16ce1162dbd0caf9fb2
kecyGaurab <kecy.gaurab@gmail.com>
2020-10-14 10:34:36 +0300

    initiated new vue project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  @{{ user.username }} - {{ fullName }}
+  <strong>Followers:</strong> {{ followers }}
+  <button @click ="followUser"> Follow </button>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'kecy',
+        firstName: 'Gaurab',
+        lastName: 'kecy',
+        email: 'kecy.gaurab@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  computed:{
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods:{
+    followUser(){
+      this.followers++
+    }
   }
 }
 </script>
@@ -19,8 +41,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 </style>
